8f7a81be6aee70db2a96fea91ab82a19ef88a48b
Maya Oniciuc <maya.klmno@gmail.com>
2024-12-16 09:59:56 -0800

    this is my current code, but it has an error under multiple lines now. Saying "Cannot access 'java. io. Serializable' which is a supertype of 'kotlin. Int'. Check your module classpath for missing or conflicting dependencies"
    so need to fix and figure out what I did wrong

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,24 +1,38 @@
-fun main() {
+import kotlin.math.absoluteValue
 
-    fun part2(input: List<String>): Int {
+fun main() {
+    fun part1(input: List<String>): Int {
         var leftNums = mutableListOf<Int>()
         var rightNums = mutableListOf<Int>()
-        rightNums.sort()
-        leftNums.sort()
+        rightNums.sorted()
+        leftNums.sorted()
+        // use this section from the video Kotlin by JetBrains//
+        val lines = readInput("day01.txt")
+        val (leftNums, rightNums ) = lines.map { line ->
+            val first = line.substringBefore(" ").toInt()
+            val second = line.substringAfterLast(" ").toInt()
+            first to second
+        }.unzip()
+        // end of the code used from the video kotlin by jetbrains day 1//
 
         var totalDistance = 0
-        for (i in leftNums){
-            var currentIndex = leftNums.indexOf(i)
-            totalDistance = i + rightNums.get(currentIndex)
+        for (i in leftNums.indices){
+            //var currentIndex = leftNums.indexOf(i)
+            //totalDistance = i + rightNums[currentIndex]
+            var difference = (rightNums[i]-leftNums[i]).absoluteValue
+            totalDistance += difference
         }
         return totalDistance
-
     }
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     //part1(input).println()
-    part2(input).println()
+    part1(input).println()
+}
+
+
+
+    // Read the input from the `src/Day01.txt` file.
+
 
 //    fun part1(input: List<String>): Int {
 //        println(input)
@@ -71,4 +85,5 @@ fun main() {
 //    val input = readInput("Day01")
 //    //part1(input).println()
 //    part2(input).println()
-}
+
+
